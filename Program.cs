97740e24cda390034343b5a2a796b21ca7464b7d
Claudiana Kindermann <56567665+claudianakindermann@users.noreply.github.com>
2022-07-25 17:43:13 -0300

    Calculadora

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("v1 = " + v1);
+            Console.WriteLine("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("v2 = " + v2);
+            Console.Write("Resultado da soma:" + Soma(v1, v2));
+        }
+
+        private static float Soma(float v1, float v2)
+        {
+            return v1 + v2;
+        }
+    }
+
+}
